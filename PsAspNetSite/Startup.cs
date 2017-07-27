69525f763415d64361c5d963bb1df7c20d5050fa
David McGowan <david.mcgowan@screenmedia.co.uk>
2017-07-27 17:13:02 +0100

    Add project files.

---
+++ PsAspNetSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PsAspNetSite.Startup))]
+namespace PsAspNetSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
